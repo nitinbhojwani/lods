a5ef49f2ce8963e1acc22e630588c9e8e66cb945
nbhojwani <nbhojwani@vmware.com>
2017-10-18 13:57:57 +0530

    add Trie data structure

---
+++ python/trie/Trie.py
@@ -0,0 +1,29 @@
+class Node:
+    def __init__(self, value=None):
+        self.children = {}
+        self.value = value
+        is_leaf = False
+
+class Trie:
+    def __init__(self):
+        self.root = Node()
+
+    def insert(self, word):
+        curr_node = self.root
+        if not word:
+            return
+        for i in range(len(word)):
+            if not word[i] in curr_node.children:
+                curr_node.children[word[i]] = Node(word[i])
+            curr_node = curr_node.children[word[i]]
+        curr_node.is_leaf = True
+
+    def look_up(self, word):
+        curr_node = self.root
+        if not word:
+            return False
+        for i in range(len(word)):
+            if not word[i] in curr_node.children:
+                return False
+            curr_node = curr_node.children[word[i]]
+        return curr_node.is_leaf
